16f09ee1aa3fd9c818e9c043fe1b2afdcc2ee7c1
Elia <116923946+Elia-Ayoub@users.noreply.github.com>
2025-04-05 14:04:40 +0200

    change website

--- data_scraper.sh
+++ data_scraper.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-URL="https://www.veracash.com/gold-price-and-chart"
+#URL="https://www.veracash.com/gold-price-and-chart"
+URL="https://atkinsonsbullion.com/charting"
 
 DATA_FILE="/home/ubuntu/Data-Scraping-Dashboard-Project/gold_prices.csv"
 
 # Fetch the webpage content and save it to a temporary file
 curl -s "$URL" > temp.html
 
-# Extract "US$2,984.68 / oz" then isolate just the numeric portion 
-PRICE=$(grep -oP 'US\$[0-9,]+\.[0-9]+\s*/\s*oz' temp.html | sed -E 's/US\$| \/ oz//g' | sed 's/,//g')
+# Extract the $ price then isolate just the numeric portion 
+PRICE=$(grep -oP '\$[ \t]*[0-9,]+\.[0-9]+\s*/\s*oz' temp.html | sed -E 's/\$[[:space:]]*//; s/\/[[:space:]]*oz//; s/,//g')
 
 
 # Get the current timestamp
